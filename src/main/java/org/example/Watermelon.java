3a68172ecff0063c98e1a885a5238d398e3239cc
Mahmoud Hassan <MahmoudHassan@example.com>
2023-10-01 17:08:13 +0300

    Watermelon

---
+++ src/main/java/org/example/Watermelon.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Watermelon {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int input = scan.nextInt();
+
+        if(input<=2){
+            System.out.println("NO");
+            return;
+        }
+
+        if(input%2==0)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+
+    }
+}
