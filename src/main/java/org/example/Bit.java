ee9f59a140df7a1bcf3fb59d13378e4090ff9e91
Mahmoud Hassan <MahmoudHassan@example.com>
2024-02-21 23:24:17 +0200

    bit

---
+++ src/main/java/org/example/Bit.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Scanner;
+
+//282A
+public class Bit {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int result = 0;
+        int n = scan.nextInt();
+
+        for (int i = 0; i<=n; i++) {
+            String input = scan.nextLine();
+            if(input.contains("++")){
+                result++;
+            }
+            if(input.contains("--")){
+                result--;
+            }
+        }
+        System.out.println(result);
+    }
+}
