a688bc2df662f61e1dba9ae6202e21b91deda09a
Mahmoud Hassan <MahmoudHassan@example.com>
2024-02-22 20:18:00 +0200

    Boy or Girl

---
+++ src/main/java/org/example/BoyOrGirl.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.*;
+
+//236A
+public class BoyOrGirl {
+    public static void main(String[] args) {
+        String word = new Scanner(System.in).next();
+        Set<Character> alphabet = new HashSet<>();
+        for (char c : word.toCharArray()) {
+            alphabet.add(c);
+        }
+
+        if (alphabet.size() % 2 == 0)
+            System.out.println("CHAT WITH HER!");
+        else
+            System.out.println("IGNORE HIM!");
+    }
+}
