abc1b83c727bbf132d94caa1752fd49a479b9d4a
Mahmoud Hassan <mshmoudghania@gmail.com>
2024-02-24 19:03:39 +0200

    Twins

--- src/main/java/org/example/D900/Twins.java
+++ src/main/java/org/example/D900/Twins.java
@@ -1,8 +1,36 @@
 package org.example.D900;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 //160A
 public class Twins {
     public static void main(String[] args) {
         //Football
+        Scanner scan = new Scanner(System.in);
+
+        int count = scan.nextInt();
+        int [] coins = new int[count];
+
+        for (int i =0; i<count;i++){
+            coins[i]=scan.nextInt();
+        }
+
+        Arrays.sort(coins);
+        int sumOfPicked = 0;
+
+        for (int i = coins.length-1;i>=0;i--){
+            sumOfPicked+=coins[i];
+            int sumOfRest = 0;
+            for (int j =0;j<i;j++){
+                sumOfRest+=coins[j];
+            }
+            if (sumOfPicked>sumOfRest){
+                System.out.println(count-i);
+                break;
+            }
+        }
+
+
     }
 }
