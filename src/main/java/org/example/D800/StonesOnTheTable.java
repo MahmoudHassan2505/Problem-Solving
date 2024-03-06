03a01147e15af9b89da8a27add1df7c52d4c28a7
Mahmoud Hassan <mshmoudghania@gmail.com>
2024-03-07 00:24:06 +0200

    A.Stones On The Table

--- src/main/java/org/example/D800/StonesOnTheTable.java
+++ src/main/java/org/example/D800/StonesOnTheTable.java
@@ -3,7 +3,7 @@ package org.example.D800;
 import java.util.Scanner;
 
 //266A
-public class StonesOnTheTableNotSolved {
+public class StonesOnTheTable {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int numOfColor = scan.nextInt();
@@ -11,11 +11,15 @@ public class StonesOnTheTableNotSolved {
         char current;
         char next;
         int count =0;
-        for (int i=0;i<numOfColor;i++){
+        for (int i=0;i<numOfColor-1;i++){
             current = row.charAt(i);
-
-
+            next = row.charAt(i+1);
+            if (current==next){
+                count++;
+            }
         }
+
+        System.out.println(count);
 
     }
 }
